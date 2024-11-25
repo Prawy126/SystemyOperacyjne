ad869b9aa39bccf5cd380223715136f486ef9747
Prawy_126 <michal190713@gmail.com>
2024-11-25 18:26:27 +0100

    Ostatni szlif

--- Projekt1/fork.asm
+++ Projekt1/fork.asm
@@ -1,124 +1,113 @@
 section .data
-    child_msg db "This is the child process. PID: "
-    child_msg_len equ $ - child_msg
+    child_msg db "This is the child process. PID: " ; Wiadomość dla procesu dziecka
+    child_msg_len equ $ - child_msg               ; Długość wiadomości dziecka
 
-    parent_msg db "This is the parent process. PID: "
-    parent_msg_len equ $ - parent_msg
+    parent_msg db "This is the parent process. PID: " ; Wiadomość dla procesu rodzica
+    parent_msg_len equ $ - parent_msg                 ; Długość wiadomości rodzica
 
-    child_pid_msg db ", Child PID: "
-    child_pid_msg_len equ $ - child_pid_msg
+    child_pid_msg db ", Child PID: " ; Separator między PID rodzica a PID dziecka
+    child_pid_msg_len equ $ - child_pid_msg ; Długość separatora
 
-    new_line db 0x0a ; ASCII value for newline
+    new_line db 0x0a ; Znak nowej linii (ASCII)
 
 section .bss
-    pid resq 1      ; Space for child's PID
-    buf resb 20     ; Buffer for storing PID as string
+    pid resq 1  ; Przechowuje PID dziecka
+    buf resb 20 ; Bufor dla konwersji liczby na tekst
 
 section .text
     global _start
 
 _start:
-    ; Wywołanie fork
-    mov eax, 57          ; Numer syscall dla fork
-    syscall
+    mov eax, 57          ; Załaduj numer syscall dla fork do rejestru eax
+    syscall              ; Wykonaj syscall (tworzy nowy proces)
 
-    ; Sprawdzenie wyniku fork (dziecko czy rodzic)
-    test rax, rax
-    jz child_process     ; Jeśli wynik to 0, to proces dziecka
+    test rax, rax        ; Sprawdź wynik fork (czy rax jest 0)
+    jz child_process     ; Skocz do child_process, jeśli wynik to 0 (proces dziecka)
 
-    ; Kod procesu rodzica
     mov [pid], rax       ; Zapisz PID dziecka w zmiennej pid
 
-    ; Czekaj na zakończenie procesu dziecka
-    mov rax, 61          ; Numer syscall dla wait4
-    mov rdi, [pid]       ; PID dziecka
-    mov rsi, 0           ; status = NULL
-    mov rdx, 0           ; options = 0
-    mov r10, 0           ; rusage = NULL
-    syscall
+    mov rax, 61          ; Załaduj numer syscall dla wait4 do rejestru eax
+    mov rdi, [pid]       ; Ustaw PID dziecka w rdi
+    mov rsi, 0           ; Ustaw wskaźnik na status jako NULL
+    mov rdx, 0           ; Ustaw opcje jako 0
+    mov r10, 0           ; Ustaw rusage jako NULL
+    syscall              ; Wykonaj syscall (czeka na zakończenie procesu dziecka)
 
-    ; Teraz rodzic wykonuje swoje operacje wyjścia
-    mov rdi, 1           ; Deskryptor pliku 1 (stdout)
+    mov rdi, 1           ; Deskryptor pliku (stdout)
     mov rsi, parent_msg  ; Adres komunikatu rodzica
     mov rdx, parent_msg_len ; Długość komunikatu rodzica
     mov rax, 1           ; Numer syscall dla write
     syscall              ; Wyświetl komunikat rodzica
 
-    ; Wyświetl PID rodzica
-    mov rax, 39          ; Numer syscall dla getpid
-    syscall
-    mov rdi, rax         ; Przekaż PID rodzica do rdi
-    call print_int       ; Wywołaj funkcję print_int
+    mov rax, 39          ; Załaduj numer syscall dla getpid do rejestru eax
+    syscall              ; Pobierz PID procesu rodzica
+    mov rdi, rax         ; Przekaż PID do rdi
+    call print_int       ; Wywołaj funkcję konwersji i wyświetlenia liczby
 
-    ; Wyświetl ", Child PID: "
-    mov rdi, 1           ; Deskryptor pliku 1 (stdout)
-    mov rsi, child_pid_msg ; Separator
+    mov rdi, 1           ; Deskryptor pliku (stdout)
+    mov rsi, child_pid_msg ; Separator dla PID dziecka
     mov rdx, child_pid_msg_len ; Długość separatora
     mov rax, 1           ; Numer syscall dla write
     syscall              ; Wyświetl separator
 
-    ; Wyświetl PID dziecka
-    mov rax, [pid]       ; Załaduj PID dziecka
-    mov rdi, rax         ; Przekaż PID dziecka do rdi
-    call print_int       ; Wywołaj funkcję print_int
+    mov rax, [pid]       ; Załaduj PID dziecka z pamięci
+    mov rdi, rax         ; Przekaż PID do rdi
+    call print_int       ; Wywołaj funkcję konwersji i wyświetlenia liczby
 
-    ; Wyświetl nową linię
-    mov rdi, 1           ; Deskryptor pliku 1 (stdout)
+    mov rdi, 1           ; Deskryptor pliku (stdout)
     mov rsi, new_line    ; Znak nowej linii
-    mov rdx, 1           ; Długość znaku nowej linii (1 bajt)
+    mov rdx, 1           ; Długość znaku nowej linii
     mov rax, 1           ; Numer syscall dla write
     syscall              ; Wyświetl nową linię
 
-    jmp exit             ; Przejdź do wyjścia
+    jmp exit             ; Skocz do sekcji exit
 
 child_process:
-    ; Kod procesu dziecka
-    mov rdi, 1
-    mov rsi, child_msg
-    mov rdx, child_msg_len
-    mov rax, 1
-    syscall
-
-    ; Wyświetl PID dziecka
-    mov rax, 39          ; Numer syscall dla getpid
-    syscall
-    mov rdi, rax         ; Przekaż PID dziecka do rdi
-    call print_int       ; Wywołaj funkcję print_int
-
-    ; Wyświetl nową linię
-    mov rdi, 1           ; Deskryptor pliku 1 (stdout)
-    mov rsi, new_line    ; Znak nowej linii
-    mov rdx, 1           ; Długość znaku nowej linii (1 bajt)
+    mov rdi, 1           ; Deskryptor pliku (stdout)
+    mov rsi, child_msg   ; Adres komunikatu dziecka
+    mov rdx, child_msg_len ; Długość komunikatu dziecka
     mov rax, 1           ; Numer syscall dla write
-    syscall
+    syscall              ; Wyświetl komunikat dziecka
+
+    mov rax, 39          ; Załaduj numer syscall dla getpid
+    syscall              ; Pobierz PID procesu dziecka
+    mov rdi, rax         ; Przekaż PID do rdi
+    call print_int       ; Wywołaj funkcję konwersji i wyświetlenia liczby
+
+    mov rdi, 1           ; Deskryptor pliku (stdout)
+    mov rsi, new_line    ; Znak nowej linii
+    mov rdx, 1           ; Długość znaku nowej linii
+    mov rax, 1           ; Numer syscall dla write
+    syscall              ; Wyświetl nową linię
 
 exit:
-    ; Zakończ program
-    mov rax, 60          ; Numer syscall dla exit
-    mov rdi, 0           ; Kod wyjścia 0 (sukces)
-    syscall
+    mov rax, 60          ; Załaduj numer syscall dla exit
+    mov rdi, 0           ; Kod wyjścia (0 - sukces)
+    syscall              ; Zakończ proces
 
 print_int:
-    ; Konwersja liczby w rdi na ciąg znaków
-    mov rax, rdi         ; Umieść liczbę do konwersji w rax
-    mov rcx, 10          ; Ustaw bazę (10) dla konwersji dziesiętnej
+    mov rax, rdi         ; Przenieś liczbę do rax
+    mov rcx, 10          ; Ustaw bazę dziesiętną
     lea rdi, [buf + 20]  ; Ustaw wskaźnik na koniec bufora
-    mov byte [rdi], 0    ; Dodaj znak null na końcu bufora
+    mov byte [rdi], 0    ; Dodaj znak null na końcu
 
 print_int_loop:
-    dec rdi
-    xor rdx, rdx         ; Wyzeruj rdx
-    div rcx              ; Podziel rax przez 10
-    add dl, '0'          ; Konwertuj cyfrę na ASCII
+    dec rdi              ; Przesuń wskaźnik w lewo
+    xor rdx, rdx         ; Wyzeruj rdx (reszta dzielenia)
+    div rcx              ; Podziel przez 10
+    add dl, '0'          ; Zamień cyfrę na ASCII
     mov [rdi], dl        ; Zapisz cyfrę w buforze
-    test rax, rax        ; Sprawdź, czy rax jest zerem
+    test rax, rax        ; Sprawdź, czy liczba to 0
     jnz print_int_loop   ; Jeśli nie, kontynuuj pętlę
 
-    ; Wyświetl liczbę
-    mov rsi, rdi         ; Ustaw rsi na początek przetworzonego ciągu
-    mov rdx, buf + 20    ; Ustaw rdx na koniec bufora
+    mov rsi, rdi         ; Ustaw wskaźnik na początek ciągu
+    mov rdx, buf + 20    ; Ustaw wskaźnik na koniec bufora
     sub rdx, rsi         ; Oblicz długość ciągu
     mov rax, 1           ; Numer syscall dla write
-    mov rdi, 1           ; Deskryptor pliku 1 (stdout)
-    syscall
-    ret
+    mov rdi, 1           ; Deskryptor pliku (stdout)
+    syscall              ; Wyświetl ciąg
+    ret                  ; Zakończ funkcję
+
+;nasm -f elf64 ASMwrite.asm -o ASMwrite.o
+
+;ld ASMwrite.o -o ASMwrite
--- Projekt1/fork.c
+++ Projekt1/fork.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    pid_t pid;
+    pid_t pid; // Zmienna do przechowywania PID procesu
 
     // Wywołanie funkcji fork
     pid = fork();
 
-    if (pid < 0) { // Błąd przy wywołaniu fork
+    if (pid < 0) { // Błąd podczas tworzenia procesu
         perror("fork failed");
         return 1;
-    } else if (pid == 0) { // Proces dziecka
+    } else if (pid == 0) { // Kod procesu dziecka
         printf("This is the child process. PID: %d\n", getpid());
-    } else { // Proces rodzica
+    } else { // Kod procesu rodzica
+        // Czekaj na zakończenie procesu dziecka
+        waitpid(pid, NULL, 0);
+
+        // Wyświetl komunikat rodzica z PID dziecka
         printf("This is the parent process. PID: %d, Child PID: %d\n", getpid(), pid);
     }
 
-    return 0;
+    return 0; // Zakończ program
 }
